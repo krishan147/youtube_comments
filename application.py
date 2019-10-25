9423f75b0754702a84015da62e608b5a27e99202
krishan vadher <krishan.vadher@gmail.com>
2019-10-25 14:00:49 +0100

    Added link

---
+++ application.py
@@ -0,0 +1,27 @@
+import google.oauth2.credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+
+
+def get_authenticated_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_console()
+    return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+
+if __name__ == '__main__':
+    # When running locally, disable OAuthlib's HTTPs verification. When
+    # running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    service = get_authenticated_service()
+
+# https://python.gotrained.com/youtube-api-extracting-comments/
